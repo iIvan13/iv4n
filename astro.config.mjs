e862f6b879f1cfc091a10b22e3a8b0d8930e182f
iIvan13 <todelanoivan13@gmail.com>
2024-06-15 18:24:22 -0500

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'es'],
-        routing: {
-            prefixDefaultLocale: false,
-        }
-    },
+  output: 'server',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  adapter: netlify()
 });
